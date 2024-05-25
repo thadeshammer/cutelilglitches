fe1c50e97f61273507ae95572d613536778f6f32
thades <thadeshammer@gmail.com>
2024-05-25 11:49:20 -0400

    WORKING TRANSPARENCY and sample animation custom artwork finally

--- public/index.js
+++ public/index.js
@@ -1,5 +1,8 @@
 import Phaser from "phaser";
 
+/**
+ * @type {Phaser.config}
+ */
 const config = {
   type: Phaser.AUTO,
   width: 1920,
@@ -20,9 +23,11 @@ const config = {
     noAudio: true,
   },
   render: {
-    pixelArt: false,
-    antialias: true,
+    pixelArt: true,
+    antialias: false,
   },
+  backgroundColor: "rgba(0,0,0,0)",
+  transparent: true,
 };
 
 const game = new Phaser.Game(config);
@@ -30,27 +35,27 @@ const game = new Phaser.Game(config);
 let flowers = [];
 
 function preload() {
-  this.load.image("flower", "assets/placeholder.png");
+  this.load.image("flower", "assets/flower_v1_transparency.png");
 }
 
 function create() {
-  // spat this out with chatgpt just to see a concrete example
-  // TODO we want them on the ground (not flying) and moving left to right
-  // maybe multiple floor heights
   const numberOfSprites = 10;
   const screenHeight = this.sys.game.config.height;
   const screenWidth = this.sys.game.config.width;
 
   for (let i = 0; i < numberOfSprites; i++) {
+    /**
+     * @type {Phaser.Physics.Arcade.Sprite}
+     */
+    let sprite = this.physics.add.sprite(0, 0, "flower");
     let x = Phaser.Math.Between(0, screenWidth);
-    let y = Phaser.Math.Between(screenHeight - 100, screenHeight - 50); // Bottom of the screen
-    let sprite = this.physics.add.sprite(x, y, "flower");
+    let height_offset = screenHeight - sprite.height;
+    let y = Phaser.Math.Between(height_offset - 60, height_offset - 20);
+    sprite.setPosition(x, y);
     sprite.setCollideWorldBounds(true);
+    sprite.setScale(3.0, 3.0);
     sprite.setBounce(1);
-    sprite.setVelocity(
-      Phaser.Math.Between(-100, 100),
-      Phaser.Math.Between(-100, 100)
-    );
+    sprite.setVelocity(Phaser.Math.Between(-100, 0), 0);
     flowers.push(sprite);
   }
 }
@@ -59,9 +64,6 @@ function update() {
   flowers.forEach((sprite) => {
     if (sprite.body.blocked.left || sprite.body.blocked.right) {
       sprite.setVelocityX(sprite.body.velocity.x * -1);
-    }
-    if (sprite.body.blocked.up || sprite.body.blocked.down) {
-      sprite.setVelocityY(sprite.body.velocity.y * -1);
     }
   });
 }
