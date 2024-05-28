5bc3481614b7476a8d6a6dd47e4a2844a1b8909b
thades <thadeshammer@gmail.com>
2024-05-28 11:17:59 -0400

    electron stands up in windows and wsl DOES NOT PUBLISH TO LOCALHOST

---
+++ switch_to_windows.sh
@@ -0,0 +1,3 @@
+# switch-to-windows.sh
+mv node_modules node_modules_wsl
+mv node_modules_windows node_modules
---
+++ switch_to_wsl.sh
@@ -0,0 +1,3 @@
+# switch-to-wsl.sh
+mv node_modules node_modules_windows
+mv node_modules_wsl node_modules
